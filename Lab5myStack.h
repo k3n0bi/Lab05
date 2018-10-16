897c3cd410fb9d0d3f0b09be397849797a9ce078
k3n0bi <43155065+k3n0bi@users.noreply.github.com>
2018-10-15 18:11:19 -0600

    Create Lab5myStack.h

---
+++ Lab5myStack.h
@@ -0,0 +1,30 @@
+// Lab5myStack.h
+// Ben Howe
+// COSC 2030
+// Lab 5
+
+#ifndef LAB5MYSTACK_H
+#define LAB5MYSTACK_H
+
+#include <vector>
+//#include <list>
+using std::vector;
+//using std::list;
+
+class MyStack
+{
+public:
+	MyStack();
+	~MyStack();
+	bool isEmpty() const;
+	void push(char& c);
+	char pull();
+private:
+	vector <char> myVector;
+	//list <char> myVector;
+};
+
+
+
+#endif 
+
