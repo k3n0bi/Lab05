3cadd06e651e84f1a27deb8fa9798d29cce77917
k3n0bi <43155065+k3n0bi@users.noreply.github.com>
2018-10-22 06:05:41 -0600

    Update Lab5Driver.cpp

--- Lab5Driver.cpp
+++ Lab5Driver.cpp
@@ -2,6 +2,7 @@
 // Ben Howe
 // COSC 2030
 // Lab 5
+// October 22, 2018
 
 #include <iostream>
 #include <string>
@@ -15,6 +16,7 @@ using std::string;
 using std::stack;
 using std::vector;
 using std::list;
+using std::cin;
 using std::cout;
 using std::endl;
 
@@ -28,133 +30,144 @@ string stringReversal4(string stringinput);  // String reversal using Mystack
 
 int main()
 {
-	//Part 1 not requried for lab.
+  //Part 1 not requried for lab.
 
-	string teststring = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	cout << "Starting String: " << teststring << endl;
+  string teststring;  
 
-	//Part 2.
-	cout << "Part 2" << endl;
-	cout << "Stack reversed: " << stringReversal1(teststring) << endl;
+  cout << "Enter a string to test (enter nothing to quit): ";
+  getline(cin, teststring);
 
-	//Part 3.
-	cout << "Part 3" << endl;
-	cout << "Vector reversed: " << stringReversal2(teststring) << endl;
-
-	//Part 4.
-	cout << "Part 4" << endl;
-	cout << "List reversed: " << stringReversal3(teststring) << endl;
-
-	//Part 5.
-	cout << "Part 5" << endl;
-	cout << "Mystack reversed: " << stringReversal4(teststring) << endl;
+  while (!teststring.empty())
+  {
 
 
-	system("Pause");
-	return 0;
+    cout << "Starting String: " << teststring << endl;
+
+    //Part 2.
+    cout << "Part 2" << endl;
+    cout << "Stack reversed: " << stringReversal1(teststring) << endl;
+
+    //Part 3.
+    cout << "Part 3" << endl;
+    cout << "Vector reversed: " << stringReversal2(teststring) << endl;
+
+    //Part 4.
+    cout << "Part 4" << endl;
+    cout << "List reversed: " << stringReversal3(teststring) << endl;
+
+    //Part 5.
+    cout << "Part 5" << endl;
+    cout << "Mystack reversed: " << stringReversal4(teststring) << endl;
+
+    cout << "==============================================" << endl;
+    cout << "Enter a string to test (enter nothing to quit): ";
+    getline(cin, teststring);
+  }
+
+  return 0;
 }
 
 string stringReversal1(string stringinput) // String reversal using a stack.
 {
-	/*
-	(a) bool empty() const;
-	(b) void pop();
-	(c) void push(char& c);
-	(d) char& top();
-	*/
-	string stringoutput;
-	stack <char> fatstack;
+  /*
+  (a) bool empty() const;
+  (b) void pop();
+  (c) void push(char& c);
+  (d) char& top();
+  */
+  string stringoutput;
+  stack <char> fatstack;
 
-	// Populate the stack
-	for (int i = 0; i < stringinput.length(); i++)
-	{
-		//cout << stringinput.at(i) << endl;
-		fatstack.push(stringinput.at(i));
-	}
-	// Pop characters off the stack into the output
+  // Populate the stack
+  for (int i = 0; i < stringinput.length(); i++)
+  {
+    //cout << stringinput.at(i) << endl;
+    fatstack.push(stringinput.at(i));
+  }
+  // Pop characters off the stack into the output
 
-	while (!fatstack.empty())
-	{
-		//cout << fatstack.top() << endl;
-		stringoutput += fatstack.top();
-		fatstack.pop();
-	}
-	return stringoutput;
+  while (!fatstack.empty())
+  {
+    //cout << fatstack.top() << endl;
+    stringoutput += fatstack.top();
+    fatstack.pop();
+  }
+  return stringoutput;
 }
 
 string stringReversal2(string stringinput)  // String reversal using a vector.
 {
-	/*
-	(a) bool empty() const;
-	(b) void pop_back();
-	(c) void push_back(char& c);
-	(d) char& back();
-	*/
-	string stringoutput;
-	vector <char> vec;
+  /*
+  (a) bool empty() const;
+  (b) void pop_back();
+  (c) void push_back(char& c);
+  (d) char& back();
+  */
+  string stringoutput;
+  vector <char> vec;
 
-	// Populate the vector
-	for (int i = 0; i < stringinput.length(); i++)
-	{
-		//cout << stringinput.at(i) << endl;
-		vec.push_back(stringinput.at(i));
-	}
-	// Pop characters off the stack into the output
+  // Populate the vector
+  for (int i = 0; i < stringinput.length(); i++)
+  {
+    //cout << stringinput.at(i) << endl;
+    vec.push_back(stringinput.at(i));
+  }
+  // Pop characters off the stack into the output
 
-	while (!vec.empty())
-	{
-		stringoutput += vec.back();
-		vec.pop_back();
-	}
-	return stringoutput;
+  while (!vec.empty())
+  {
+    stringoutput += vec.back();
+    vec.pop_back();
+  }
+  return stringoutput;
 }
 string stringReversal3(string stringinput) // String reversal using a list.
 {
-	/*
-	(a) bool empty() const;
-	(b) void pop_back();
-	(c) void push_back(char& c);
-	(d) char& back();
-	*/
-	string stringoutput;
-	list <char> myList;
+  /*
+  (a) bool empty() const;
+  (b) void pop_back();
+  (c) void push_back(char& c);
+  (d) char& back();
+  */
+  string stringoutput;
+  list <char> myList;
 
-	// Populate the vector
-	for (int i = 0; i < stringinput.length(); i++)
-	{
-		//cout << stringinput.at(i) << endl;
-		myList.push_back(stringinput.at(i));
-	}
-	// Pop characters off the stack into the output
+  // Populate the vector
+  for (int i = 0; i < stringinput.length(); i++)
+  {
+    //cout << stringinput.at(i) << endl;
+    myList.push_back(stringinput.at(i));
+  }
+  // Pop characters off the stack into the output
 
-	while (!myList.empty())
-	{
-		stringoutput += myList.back();
-		myList.pop_back();
-	}
-	return stringoutput;
+  while (!myList.empty())
+  {
+    stringoutput += myList.back();
+    myList.pop_back();
+  }
+  return stringoutput;
 }
 string stringReversal4(string stringinput)
 {
-	/*
-	(a) bool isEmpty() const;
-	(b) void push(char& c);
-	(c) char pull();
-	*/
-	string stringoutput;
-	MyStack anotherstack;
-	
-	// Populate the MyStack
-	for (int i = 0; i < stringinput.length(); i++)
-	{
-		//cout << stringinput.at(i) << endl;
-		anotherstack.push(stringinput.at(i));
-	}
-	// Pop characters off the stack into the output
+  /*
+  (a) bool isEmpty() const;
+  (b) void push(char& c);
+  (c) char pull();
+  */
+  string stringoutput;
+  MyStack anotherstack;
 
-	while (!anotherstack.isEmpty())
-	{
-		stringoutput += anotherstack.pull(); // Pull a character off MyStack
-	}
-	return stringoutput;
+  // Populate the MyStack
+  for (int i = 0; i < stringinput.length(); i++)
+  {
+    //cout << stringinput.at(i) << endl;
+    anotherstack.push(stringinput.at(i));
+  }
+  // Pop characters off the stack into the output
+
+  while (!anotherstack.isEmpty())
+  {
+    stringoutput += anotherstack.pull(); // Pull a character off MyStack
+  }
+  return stringoutput;
 }
